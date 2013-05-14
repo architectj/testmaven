b0df43a4a2c53d1f5b7bb45cb6605ff0c81ee7cc
deity <deity98@gmail.com>
2013-05-15 08:01:15 +0900

    테스트파일 추가했는데 maven에서 테스트가 안됨;;

---
+++ src/test/java/net/deity/test.java
@@ -0,0 +1,18 @@
+package net.deity;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kimgyupyo
+ * Date: 13. 5. 14.
+ * Time: 오후 8:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+}
