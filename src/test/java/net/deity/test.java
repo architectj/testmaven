3d79525daa716ed7ab25c1e6a031e4b992adc5c9
deity <deity98@gmail.com>
2013-05-15 09:07:18 +0900

    mysql 접속테스트

--- src/test/java/net/deity/test.java
+++ src/test/java/net/deity/test.java
@@ -2,6 +2,10 @@ package net.deity;
 
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kimgyupyo
@@ -13,6 +17,22 @@ public class test {
 
     @Test
     public void test(){
-        System.out.println("test");
+        Connection conn;
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+
+            System.out.println("success");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (InstantiationException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 }
